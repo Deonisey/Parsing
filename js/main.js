b491b465d86e613651c2c006c447336bf93a34ad
Deonisey <48990894+Deonisey@users.noreply.github.com>
2019-04-26 19:58:59 +0300

    UPDT

--- js/main.js
+++ js/main.js
@@ -1,24 +1,54 @@
-(function($){
-	
-	function parserGo(){
+(function($)
+{
+	var list = [];
+	function parserGo()
+	{
+		
 	  var b = $.ajax('https://youla.ru/' + $('#city').val() +'?q=' + $('#find').val());
-	  b.done(function (d) {
-		analysisSite(d);
-	  });
-	  b.fail(function (e, g, f) {
-		alert('Fail');
-	  })
+	  
+		b.done
+		(
+			function (d) 
+			{
+				analysisSite(d);
+			}
+		);
+			
+		b.fail
+		(	
+			function (e, g, f) 
+			{
+				alert('Fail');
+			}
+		)
 	}
 	
-	function analysisSite(data){
-	  var res = '';
-	  $(data).find('a').each(function(){
-		res+=$(this).text()+'<P>'+$(this).attr('href')+'';
-	  })
-	  $('#resultbox').html(res);
+	function analysisSite(data)
+	{
+	
+		var res = '<P>';
+		var str = '';
+		var med = 0;
+
+		$(data).find('.product_item__description').each
+		(
+			
+			function()
+			{
+				str = $(this).text();
+				str = str.replace(/[^0-9]/gim,'');
+				list.push( Number(str) );
+				med += Number(str);
+			}
+		)
+		
+		med = med/list.length;
+		res += list + '<P>' + med
+		$('#resultbox').html(res);
 	}
 	
-	$(function(){
+	$(function()
+	{
 	  $('#starter').click(parserGo);
 	  $('#resultbox').html('');
 	});
